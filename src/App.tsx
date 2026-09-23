c1a1e4d80ea9b876627b0ba47362ea708f0b0412
zeidiniopdc-forex <zeidi.niopdc@gmail.com>
2026-09-23 18:50:30 +0330

    feat: show Coachino welcome screen on first launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { ThemeProvider } from './context/ThemeContext';
 import Layout from './components/Layout';
+import Welcome from './pages/Welcome';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 import PromptGenerator from './pages/PromptGenerator';
@@ -14,26 +16,41 @@ import WorkoutTracker from './pages/WorkoutTracker';
 import CalendarPage from './pages/Calendar';
 import Progress from './pages/Progress';
 
+const WELCOME_KEY = 'coachino_welcome_seen_v1';
+
 function App() {
+  const [showWelcome, setShowWelcome] = useState(false);
+
+  useEffect(() => {
+    setShowWelcome(localStorage.getItem(WELCOME_KEY) !== 'true');
+  }, []);
+
+  const continueToApp = () => {
+    localStorage.setItem(WELCOME_KEY, 'true');
+    setShowWelcome(false);
+  };
+
   return (
     <ThemeProvider>
       <AppProvider>
         <HashRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/prompt" element={<PromptGenerator />} />
-              <Route path="/import" element={<ProgramImport />} />
-              <Route path="/nutrition" element={<Nutrition />} />
-              <Route path="/nutrition-import" element={<NutritionImport />} />
-              <Route path="/supplements" element={<Supplements />} />
-              <Route path="/supplement-import" element={<SupplementImport />} />
-              <Route path="/workout" element={<WorkoutTracker />} />
-              <Route path="/calendar" element={<CalendarPage />} />
-              <Route path="/progress" element={<Progress />} />
-            </Routes>
-          </Layout>
+          {showWelcome ? <Welcome onContinue={continueToApp} /> : (
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/prompt" element={<PromptGenerator />} />
+                <Route path="/import" element={<ProgramImport />} />
+                <Route path="/nutrition" element={<Nutrition />} />
+                <Route path="/nutrition-import" element={<NutritionImport />} />
+                <Route path="/supplements" element={<Supplements />} />
+                <Route path="/supplement-import" element={<SupplementImport />} />
+                <Route path="/workout" element={<WorkoutTracker />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="/progress" element={<Progress />} />
+              </Routes>
+            </Layout>
+          )}
         </HashRouter>
       </AppProvider>
     </ThemeProvider>
